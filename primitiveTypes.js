18d9a71404892d9b5b26fc4ef564cfd7ff03dac0
kong <yoursitup90@gmail.com>
2022-06-19 16:47:01 +0900

    primitve Type

---
+++ primitiveTypes.js
@@ -0,0 +1,16 @@
+//primitve Type 이란 원시적인 타입
+//예를들어 string ,number, boolean, null, underfined , NaN 등이 있다.
+//string 은 문자열
+//number 는 숫자
+//boolean 참거짓
+//underined 은 "정의되지 않음"
+//Null은 존재하지 않음"
+
+var hello;
+//hello 호출
+//undefined
+hello = null;
+//hello 호출
+//null
+
+//null과 undefined은 다르다! 정의되지 않는것과 존재하지 않는다라는건 다른의미
